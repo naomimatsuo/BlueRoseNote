582e1917e23cdd8a8c9e7c7f9c192233442d9adb
Naomi Matsuo <macha.flap@gmail.com>
2021-07-17 23:32:11 +0900

    スマホメニューの修正

--- layouts/user.vue
+++ layouts/user.vue
@@ -8,38 +8,38 @@
         </button>
       </div>
       <!-- phone humburger -->
-      <div class="phoneburger-container phoneburger-container-back">
+      <div class="phoneburger-container back" @click="closePhoneBurger">
         <div class="phoneburger">
-          <ul class="list-group rounded-0">
+          <ul class="list-group">
             <div class="d-flex justify-content-between mt-2 ml-3 bg-secondary">
               <img src="/icon-white.svg" width="25" height="25" class="mt-1" />
-              <button type="button" class="close mr-3" aria-label="Close" @click="closePhoneBurger">
+              <button type="button" class="bt-lg close mr-3" aria-label="Close" @click="closePhoneBurger">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <li class="phoneburger-item px-0 py-2 mt-2" @click="closePhoneBurger">
-              <NuxtLink to="/top" tag="button" class="btn btn-secondary btn-block rounded-0">
+              <NuxtLink to="/top" tag="button" class="btn btn-secondary btn-block">
                 <span class="float-left ml-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
                     <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
                   </svg>
                 </span>
-                <small>ホーム</small>
+                ホーム
               </NuxtLink>
             </li>
             <li class="phoneburger-item px-0 py-2" @click="closePhoneBurger">
-              <NuxtLink to="/userRecords" tag="button" class="btn btn-secondary btn-block rounded-0">
+              <NuxtLink to="/userRecords" tag="button" class="btn btn-secondary btn-block">
                 <span class="float-left ml-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 20 20">
                     <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                   </svg>
                 </span>
-                <small>記録</small>
+                記録
               </NuxtLink>
             </li>
             <li class="phoneburger-item px-0 py-2" @click="closePhoneBurger">
-              <NuxtLink to="/communities" tag="button" class="btn btn-secondary btn-block rounded-0">
+              <NuxtLink to="/communities" tag="button" class="btn btn-secondary btn-block">
                 <span class="float-left ml-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 20 20">
                     <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
@@ -47,18 +47,18 @@
                     <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
                   </svg>
                 </span>
-                <small>コミュニティ</small>
+                コミュニティ
               </NuxtLink>
             </li>
             <li class="phoneburger-item py-2 mb-2" @click="closePhoneBurger">
-              <NuxtLink to="/profile" tag="button" class="btn btn-secondary btn-block rounded-0">
+              <NuxtLink to="/profile" tag="button" class="btn btn-secondary btn-block">
                 <span class="float-left ml-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-gear" viewBox="0 0 20 20">
                     <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
                     <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
                   </svg>
                 </span>
-                <small>プロフィール</small>
+                プロフィール
               </NuxtLink>
             </li>
           </ul>
@@ -155,10 +155,12 @@
 export default {
   methods: {
     closePhoneBurger () {
-      $('.phoneburger-container').addClass('phoneburger-container-back');
+      $('.phoneburger-container').addClass('back');
+      $('.phoneburger').addClass('back');
     },
     openPhoneBurger () {
-      $('.phoneburger-container').removeClass('phoneburger-container-back');
+      $('.phoneburger-container').removeClass('back');
+      $('.phoneburger').removeClass('back');
     },
     signOut () {
       this.$cookies.removeAll();
@@ -170,12 +172,12 @@ export default {
 
 <style scoped>
 .phoneburger-container {
-  position: absolute;
+  position: fixed;
   top: 0px;
   right: 0;
-  z-index: 98;
+  z-index: 2147483646;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   pointer-events: none;
   background-color: rgba(0, 0, 0, 0.4);
   transition: all 0.5s;
@@ -186,9 +188,13 @@ export default {
   left: 0px;
   width: 16rem;
   transition: 1s;
-  z-index: 99;
+  z-index: 2147483647;
   pointer-events: auto;
   background-color: #85b8c9;
+}
+
+.phoneburger:hover {
+  background-color: #68919e;
 }
 
 .phoneburger-item {
@@ -196,7 +202,7 @@ export default {
   display: block;
 }
 
-.phoneburger-container-back {
+.back {
   display: none;
 }
 </style>
