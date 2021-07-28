77a69eba1e794b0b0a2ffa0dd0b2900aa2c1fba9
Naomi Matsuo <macha.flap@gmail.com>
2021-07-29 01:36:59 +0900

    スマホブラウザで遷移失敗したので修正

--- pages/communityTweetReply.vue
+++ pages/communityTweetReply.vue
@@ -431,7 +431,7 @@ export default {
     },
     toReply (target) {
       localStorage.setItem('targetTweet', JSON.stringify(target));
-      this.$router.go('communityTweetReply');
+      this.$router.go({ path: this.$router.currentRoute.path, force: true });
     },
     isMyFavTweet (target) {
       const item = target.likes.find((val) => { return val.clientId === this.$cookies.get('account_id'); });
