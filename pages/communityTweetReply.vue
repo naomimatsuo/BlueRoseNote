587aa4649c4c305ee46ee8a094ff7c03cdda627a
Naomi Matsuo <macha.flap@gmail.com>
2021-07-29 13:33:29 +0900

    戻る修正

--- pages/communityTweetReply.vue
+++ pages/communityTweetReply.vue
@@ -4,7 +4,7 @@
       <ul class="list-group list-group-flush">
         <li class="list-group-item">
           <div class="row mx-0">
-            <NuxtLink to="communityTweet" tag="button" class="btn btn-light rounded-circle" @click="gobackTo">
+            <NuxtLink to="/communityTweet" tag="button" class="btn btn-light rounded-circle" @click="gobackTo">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
               </svg>
