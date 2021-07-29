7633cc426cb55a6ee86d89b5702272c3c80912eb
Naomi Matsuo <macha.flap@gmail.com>
2021-07-29 13:12:17 +0900

    戻る修正

--- pages/communityTweetReply.vue
+++ pages/communityTweetReply.vue
@@ -4,11 +4,11 @@
       <ul class="list-group list-group-flush">
         <li class="list-group-item">
           <div class="row mx-0">
-            <button class="btn btn-light rounded-circle" @click="gobackTo">
+            <NuxtLink to="communityTweet" tag="button" class="btn btn-light rounded-circle" @click="gobackTo">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
               </svg>
-            </button>
+            </NuxtLink>
           </div>
         </li>
         <li class="list-group-item">
@@ -438,9 +438,6 @@ export default {
       const item = target.likes.find((val) => { return val.clientId === this.$cookies.get('account_id'); });
       if (item) { return true; }
       return false;
-    },
-    gobackTo () {
-      this.$router.push('communityTweet');
     }
   }
 }
