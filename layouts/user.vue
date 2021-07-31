baf877170480ec6036f7eaa328c02e2eb7acf2b6
Naomi Matsuo <macha.flap@gmail.com>
2021-08-01 02:25:45 +0900

    スマホメニューに記録閲覧者を追加

--- layouts/user.vue
+++ layouts/user.vue
@@ -40,6 +40,17 @@
             </NuxtLink>
           </li>
           <li class="phoneburger-item py-2" @click="closePhoneBurger">
+            <NuxtLink to="/recordReviewers" tag="button" class="btn btn-secondary btn-block">
+              <span class="float-left ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
+              </span>
+              記録閲覧者
+            </NuxtLink>
+          </li>
+          <li class="phoneburger-item py-2" @click="closePhoneBurger">
             <NuxtLink to="/communities" tag="button" class="btn btn-secondary btn-block">
               <span class="float-left ml-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 20 20">
