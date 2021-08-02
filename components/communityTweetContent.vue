061201a51609589c2129cb6132c135e337cc9ca8
Naomi Matsuo <macha.flap@gmail.com>
2021-08-02 16:49:29 +0900

    いいねボタン位置調整

--- components/communityTweetContent.vue
+++ components/communityTweetContent.vue
@@ -43,7 +43,7 @@
           <div class="d-flex justify-content-between mt-2 mx-0">
             <!-- Reply button -->
             <div>
-              <button class="btn btn-light btn-sm rounded-circle" @click="toReply(post)">
+              <button class="btn btn-light btn-sm rounded-circle ml-1" @click="toReply(post)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
                   <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z" />
                 </svg>
@@ -51,13 +51,13 @@
               <span v-if="post.replys.length > 0" class="text-primary ml-1">{{ post.replys.length }}</span>
             </div>
             <!-- Like buttons -->
-            <div>
-              <button v-if="!isMyFavTweet(post)" name="notLikeBtn" class="btn btn-light btn-sm rounded-circle" style="position:absolute; right: 25px" @click="likeThis($event, post)">
+            <div class="mr-1">
+              <button v-if="!isMyFavTweet(post)" name="notLikeBtn" class="btn btn-light btn-sm rounded-circle" @click="likeThis($event, post)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart" viewBox="0 0 16 16">
                   <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
                 </svg>
               </button>
-              <button v-if="isMyFavTweet(post)" name="likeBtn" class="btn btn-secondary btn-sm rounded-circle text-primary" style="position:absolute; right: 25px" @click="nomoreLikeThis($event, post)">
+              <button v-if="isMyFavTweet(post)" name="likeBtn" class="btn btn-secondary btn-sm rounded-circle text-primary" @click="nomoreLikeThis($event, post)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
                   <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z" />
                 </svg>
